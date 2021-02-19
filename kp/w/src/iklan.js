6c92ca0588a7a8894c5e01bcffceca75bbc4f993
Dakauann <74578069+Dakauann@users.noreply.github.com>
2021-02-19 01:21:24 -0300

    Add files via upload

---
+++ kp/w/src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/558494740630*
+║
+╚═〘  𝙏𝙧𝙖𝙨𝙝ф𝘿𝙠𝘽𝙊𝙏 〙
+`
+}
+exports.iklan = iklan
